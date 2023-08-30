3875d3ae34cbf92eaa67d0a6b13f4a44d9da076c
Mohaamad Kasim <kasimwasi111@gmail.com>
2023-08-31 03:10:18 +0530

    done

---
+++ script.js
@@ -0,0 +1,16 @@
+// // Get the buttons and content sections
+// const tabButtons = document.querySelectorAll(".btn_tab");
+// const contentSections = document.querySelectorAll(".services > div");
+
+// // Add click event listeners to the buttons
+// tabButtons.forEach((button, index) => {
+//   button.addEventListener("click", () => {
+//     // Remove the 'active' class from all buttons and content sections
+//     tabButtons.forEach((btn) => btn.classList.remove("active"));
+//     contentSections.forEach((section) => section.classList.remove("active"));
+
+//     // Add the 'active' class to the clicked button and corresponding content section
+//     button.classList.add("active");
+//     contentSections[index].classList.add("active");
+//   });
+// });
