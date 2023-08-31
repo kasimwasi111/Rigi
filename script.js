0913f9f088c658c5e7c7ad9e248f72f7fc8e4ce3
Mohaamad Kasim <kasimwasi111@gmail.com>
2023-08-31 09:45:22 +0530

    done

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-// // Get the buttons and content sections
-// const tabButtons = document.querySelectorAll(".btn_tab");
-// const contentSections = document.querySelectorAll(".services > div");
+const tabButtons = document.querySelectorAll(".btn_tab");
+const tabContents = document.querySelectorAll(
+  ".community, .courses, .consultation"
+);
 
-// // Add click event listeners to the buttons
-// tabButtons.forEach((button, index) => {
-//   button.addEventListener("click", () => {
-//     // Remove the 'active' class from all buttons and content sections
-//     tabButtons.forEach((btn) => btn.classList.remove("active"));
-//     contentSections.forEach((section) => section.classList.remove("active"));
+// Display default tab
+tabContents[0].style.display = "block";
+tabButtons[0].classList.add("active");
 
-//     // Add the 'active' class to the clicked button and corresponding content section
-//     button.classList.add("active");
-//     contentSections[index].classList.add("active");
-//   });
-// });
+// Add click event listeners to tab buttons
+tabButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    // Hide all tab contents
+    tabContents.forEach((content) => {
+      content.style.display = "none";
+    });
+
+    // Deactivate all tab buttons
+    tabButtons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    // Display the selected tab content
+    tabContents[index].style.display = "block";
+    button.classList.add("active");
+  });
+});
